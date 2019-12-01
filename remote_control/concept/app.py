865ca2e19fc12ea69bda2bc09e35ec8360318e1a
A-Siam <abdullah.khaled.kamal0@gmail.com>
2019-12-01 18:16:54 +0200

    Fix: CORS issuesolved

--- remote_control/concept/app.py
+++ remote_control/concept/app.py
@@ -1,14 +1,18 @@
 '''middle server'''
 from flask import Flask, render_template, request
 from utils import send_to_esp
+from flask_cors import CORS,cross_origin
 APP = Flask(__name__)
-
+CORS(APP)
+@cross_origin()
 @APP.route("/simple", methods=['GET', 'POST'])
 def box_value():
     ''' read vaule from user and send it to control-server'''
-    if request.method == "POST":
-        angle = request.form[request.form["source"]]
-        send_to_esp(angle)
+    #if request.method == "POST":
+        #angle = request.form[request.form["source"]]
+        #angle = request.form["angle"]
+        #send_to_esp(angle)
+    print(request.data())
     return render_template('test.html')
 
 
--- remote_control/webpage/web_interface/src/App.js
+++ remote_control/webpage/web_interface/src/App.js
@@ -1,41 +1,44 @@
 import React from 'react';
+import $ from "jquery";
 import './App.css';
 import './styles/style.css'
-import './Constants'
+import {constants} from "./Constants";
 class App extends React.Component {
+
 
   constructor(props) {
     super(props);
     this.state = {
-      image_index: 0
+      image: constants["IMAGES"][0]
     }
   }
   sendPostRequest(value) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", cosntants["ESP_URL"], true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-      angle: value
-    }));
+    $.ajax({
+      type: 'POST',
+      url: constants["ESP_URL"],
+      data: {angle: value},
+      contentType: 'application/json;',
+      crossDomain: true
+    });
   }
   // this function will be invoked whatever change happens to slider val
   onSliderChangeHandler = (event) => {
     //TODO: remove logs
     console.log(event.target.value)
-    sendPostRequest(event.target.value)
+    this.sendPostRequest(event.target.value)
+    this.setState({image: constants["IMAGES"][Math.floor(parseInt(event.target.value)/45)]})
   }
     //TODO: remove logs
   onButtonClickHandler = (event) => {
     console.log(event.target.value)
-    sendPostRequest(event.target.value)
+    this.sendPostRequest(event.target.value)
+    this.setState({image: constants["IMAGES"][Math.floor(parseInt(event.target.value)/45)]})
   }
   render() {
     return (
       <div>
-        {
-          //TODO: add image here 
-        }
-        <input className="slider" type="range" min="0" max="180" defaultValue="90" onChange={this.onSliderChangeHandler}></input>
+        <img src={this.state["image"]} alt="motor image"></img>
+        <input className="slider" type="range" min="0" max="180" defaultValue="0" onChange={this.onSliderChangeHandler}></input>
         <button type="button" onClick={this.onButtonClickHandler} value="0">0</button>
         <button type="button" onClick={this.onButtonClickHandler} value="45">45</button>
         <button type="button" onClick={this.onButtonClickHandler} value="90">90</button>
